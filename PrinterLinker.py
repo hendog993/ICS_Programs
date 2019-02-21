c1b30800ec636e259dacd51a40f76dff65d909d5
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-02-20 19:22:14 -0500

    Add files via upload

--- PrinterLinker.py
+++ PrinterLinker.py
@@ -1,5 +1,14 @@
 from tkinter import Tk, Label, Entry, Text , Button
 
+# Testing right now.
+# try:
+# 	import urlparse
+# except ModuleNotFoundError:
+# 	import urllib.parse as urlparse
+# from office365.runtime.auth.authentication_context import AuthenticationContext
+# from office365.sharepoint.client_context import ClientContext
+# from office365.sharepoint.file import File
+
 # Enter required values into a simple GUI
 # Press enter, save the values to a text file
 # Send that data to the online excel database
@@ -14,8 +23,7 @@ class GUI:
 	def run():
 		GUI.root.mainloop()
 
-
-	def fetch_data():
+	def reader():
 		# Fetches the data from the entry boxes,
 		# Clears the entry boxes
 		# Returns the relevent information in a list.
@@ -25,13 +33,30 @@ class GUI:
 			values.append(x.get())
 		for x in GUI.entries:
 			x.delete(0,"end")
-
 		return values
 
-	def post_data():
+	def writer(values):
+		# Writes the values to a file at this point.
+		with open('printerlog.txt','w') as master:
+			for x in values:
+				master.write(x)
+				master.write("\n")
+			master.write("\n")
+			master.close()
+
+	def read_and_write():
+		var = GUI.reader()
+		GUI.writer(var)
 		pass
 
-
+	def post_data():
+		# office365 validation, post to lowest column.
+		# Hold this data in a seperate secure file. Use json loading to pull.
+		url = 'https://www.dropbox.com/login'
+		username = 'yourusername'
+		password = 'yourpassword'
+		relative_url = '/sites/documentsite/Documents/filename.xlsx'
+		pass
 
 	def submit_button():
 		# Grab data from fields
@@ -40,6 +65,7 @@ class GUI:
 		# Clear contents in entry fields.
 		var = GUI.fetch_data()
 		print(var)
+		pass
 
 
 	root = Tk()
@@ -57,11 +83,11 @@ class GUI:
 	studentnetIDEntry = Entry(root, width=30)
 	studentnetIDEntry.grid(row=2, column =1)
 
-	filamentType = Label(root,text = "Filament(m)").grid(row=3, column = 0)
+	filamentType = Label(root,text = "Filament").grid(row=3, column = 0)
 	filamentEntry = Entry(root, width=30)
 	filamentEntry.grid(row=3, column =1)
 
-	lengthLabel = Label(root, text="Length").grid(row=4, column=0)
+	lengthLabel = Label(root, text="Length (m)").grid(row=4, column=0)
 	lengthEntry = Entry(root, width=30)
 	lengthEntry.grid(row=4, column=1)
 
@@ -73,7 +99,7 @@ class GUI:
 	descriptionEntry = Entry(root, width=30)
 	descriptionEntry.grid(row=6, column=1)
 
-	submit_button = Button(root, text="Submit", command=submit_button).grid(row=6, column=2)
+	submit_button = Button(root, text="Submit", command=read_and_write).grid(row=6, column=2)
 
 	entries = [printerEntry, studentNameEntry, studentnetIDEntry, \
 				filamentEntry,lengthEntry,  staffHelperEntry,
